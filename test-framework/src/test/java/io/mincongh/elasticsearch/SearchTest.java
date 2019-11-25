88c22a43040605684e52b8c0cf0b3fe7ff5858f5
Mincong HUANG <mincong.h@gmail.com>
2019-11-25 22:27:18 +0100

    Add more queries

--- test-framework/src/test/java/io/mincongh/elasticsearch/SearchTest.java
+++ test-framework/src/test/java/io/mincongh/elasticsearch/SearchTest.java
@@ -1,12 +1,11 @@
 package io.mincongh.elasticsearch;
 
-import java.util.HashMap;
-import java.util.Map;
 import org.elasticsearch.action.bulk.BulkItemResponse;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.support.WriteRequest.RefreshPolicy;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.search.SearchHits;
@@ -15,12 +14,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Tests Search API.
+ * Tests Search APIs.
  *
  * @author Mincong Huang
- * @see <a
- *     href="https://www.elastic.co/guide/en/elasticsearch/client/java-rest/current/java-rest-high-search.html">Search
- *     API | Java REST Client | Elastic</a>
+ * @see <a href="https://www.elastic.co/guide/en/elasticsearch/client/java-rest/current/_search_apis.html">Search
+ *     APIs | Java REST Client | Elastic</a>
  */
 public class SearchTest extends ESSingleNodeTestCase {
 
@@ -29,25 +27,50 @@ public class SearchTest extends ESSingleNodeTestCase {
   public void setUp() throws Exception {
     super.setUp();
 
-    Map<String, String> sansa = new HashMap<>();
-    sansa.put("firstName", "Sansa");
-    sansa.put("lastName", "Stark");
-
-    Map<String, String> arya = new HashMap<>();
-    arya.put("firstName", "Arya");
-    arya.put("lastName", "Stark");
-
-    Map<String, String> tyrion = new HashMap<>();
-    tyrion.put("firstName", "Tyrion");
-    tyrion.put("lastName", "Lannister");
-
     BulkResponse response =
         node()
             .client()
             .prepareBulk()
-            .add(new IndexRequest().index("users").id("sansa").source(sansa))
-            .add(new IndexRequest().index("users").id("arya").source(arya))
-            .add(new IndexRequest().index("users").id("tyrion").source(tyrion))
+            .add(new IndexRequest("users").id("sansa").source("""
+                {
+                  "firstName": "Sansa",
+                  "lastName": "Stark",
+                  "gender": "female",
+                  "house": "House Stark"
+                }
+                """, XContentType.JSON))
+            .add(new IndexRequest("users").id("arya").source("""
+                {
+                  "firstName": "Arya",
+                  "lastName": "Stark",
+                  "gender": "female",
+                  "house": "House Stark"
+                }
+                """, XContentType.JSON))
+            .add(new IndexRequest("users").id("tyrion").source("""
+                {
+                  "firstName": "Tyrion",
+                  "lastName": "Lannister",
+                  "gender": "male",
+                  "house": "House Lannister"
+                }
+                """, XContentType.JSON))
+            .add(new IndexRequest("users").id("jaime").source("""
+                {
+                  "firstName": "Jaime",
+                  "lastName": "Lannister",
+                  "gender": "male",
+                  "house": "House Lannister"
+                }
+                """, XContentType.JSON))
+            .add(new IndexRequest("users").id("cersei").source("""
+                {
+                  "firstName": "Cersei",
+                  "lastName": "Lannister",
+                  "gender": "female",
+                  "house": "House Lannister"
+                }
+                """, XContentType.JSON))
             .setRefreshPolicy(RefreshPolicy.WAIT_UNTIL)
             .execute()
             .actionGet();
@@ -58,8 +81,15 @@ public class SearchTest extends ESSingleNodeTestCase {
     }
   }
 
+  /**
+   * Search API.
+   *
+   * @see <a
+   *     href="https://www.elastic.co/guide/en/elasticsearch/client/java-rest/current/java-rest-high-search.html">Search
+   *     API | Java REST Client | Elastic</a>
+   */
   @Test
-  public void searchRequest() {
+  public void searchApi_termQueryQuery() {
     SearchResponse response =
         node()
             .client()
@@ -71,5 +101,18 @@ public class SearchTest extends ESSingleNodeTestCase {
     assertEquals(2L, hits.getTotalHits().value);
     assertEquals("sansa", hits.getHits()[0].getId());
     assertEquals("arya", hits.getHits()[1].getId());
+  }
+
+  @Test
+  public void searchApi_allMatchQuery() {
+    SearchResponse response =
+        node()
+            .client()
+            .prepareSearch("users")
+            .setQuery(QueryBuilders.matchAllQuery())
+            .get();
+
+    SearchHits hits = response.getHits();
+    assertEquals(5L, hits.getTotalHits().value);
   }
 }
