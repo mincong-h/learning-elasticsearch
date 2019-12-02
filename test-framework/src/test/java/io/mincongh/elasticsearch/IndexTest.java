58463f546e2831220e2c7acc3802350eb1328262
Mincong HUANG <mincong.h@gmail.com>
2019-12-02 22:40:13 +0100

    Add more tests for indexing

--- test-framework/src/test/java/io/mincongh/elasticsearch/IndexTest.java
+++ test-framework/src/test/java/io/mincongh/elasticsearch/IndexTest.java
@@ -1,10 +1,10 @@
 package io.mincongh.elasticsearch;
 
 import java.util.HashMap;
-import java.util.Map;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.support.replication.ReplicationResponse.ShardInfo;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.test.ESSingleNodeTestCase;
 import org.junit.Test;
@@ -20,12 +20,39 @@ import org.junit.Test;
 public class IndexTest extends ESSingleNodeTestCase {
 
   @Test
+  public void itShouldIndexWithoutDocumentId() {
+    IndexRequest idxRequest =
+        new IndexRequest("msg").source("{\"msg\":\"Hello world!\"}", XContentType.JSON);
+    IndexResponse idxResponse = client().index(idxRequest).actionGet();
+    assertEquals("msg", idxResponse.getIndex());
+    assertEquals(RestStatus.CREATED, idxResponse.status());
+    logger.info("docId={}", idxRequest.id());
+  }
+
+  @Test
+  public void itShouldIndexWithBuilder() {
+    IndexResponse idxResponse =
+        client()
+            .prepareIndex()
+            .setIndex("msg")
+            .setSource("{\"msg\":\"Hello world!\"}", XContentType.JSON)
+            .execute()
+            .actionGet();
+    assertEquals("msg", idxResponse.getIndex());
+    assertEquals(RestStatus.CREATED, idxResponse.status());
+  }
+
+  @Test
   public void indexing() {
+    HashMap<String, String> source = new HashMap<>();
+    source.put("firstName", "Sansa");
+    source.put("lastName", "Stark");
+
     IndexRequest idxRequest =
         new IndexRequest() //
             .index("users")
             .id("sansa")
-            .source(newSource());
+            .source(source);
 
     IndexResponse idxResponse = node().client().index(idxRequest).actionGet();
     assertEquals("users", idxResponse.getIndex());
@@ -39,12 +66,5 @@ public class IndexTest extends ESSingleNodeTestCase {
     assertEquals(0, shardInfo.getFailed());
     assertEquals(1, shardInfo.getSuccessful());
     assertEquals(1, shardInfo.getTotal());
-  }
-
-  private Map<String, String> newSource() {
-    Map<String, String> source = new HashMap<>();
-    source.put("firstName", "Sansa");
-    source.put("lastName", "Stark");
-    return source;
   }
 }
