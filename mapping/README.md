175980dcc6cf9d1369befa7a5efd2b7f02147130
Mincong Huang <mincong-h@users.noreply.github.com>
2020-04-04 18:04:32 +0200

    Create module Mapping (#7)
    * Test dynamic mapping
    * Test explicit mapping: create mapping when creating index
    * Test explicit mapping: add field to existing mapping

---
+++ mapping/README.md
@@ -0,0 +1,7 @@
+# Elasticsearch Mapping
+
+References:
+
+- Mapping <https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping.html>
+- Mapping Management in Index APIs <https://www.elastic.co/guide/en/elasticsearch/reference/current/indices.html>
+- Mapping Management in Index APIs (Java High Level REST Client) <https://www.elastic.co/guide/en/elasticsearch/client/java-rest/current/_index_apis.html>
---
+++ mapping/src/test/java/io/mincong/elasticsearch/ExplicitMappingTest.java
@@ -0,0 +1,74 @@
+package io.mincong.elasticsearch;
+
+import java.util.Map;
+import org.assertj.core.api.Assertions;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.admin.indices.mapping.get.GetFieldMappingsRequest;
+import org.elasticsearch.action.admin.indices.mapping.put.PutMappingRequest;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.test.ESSingleNodeTestCase;
+import org.junit.Test;
+
+/**
+ * Test explicit mapping in Elasticsearch.
+ *
+ * <p>You know more about your data than Elasticsearch can guess, so while dynamic mapping can be
+ * useful to get started, at some point you will want to specify your own explicit mappings.
+ *
+ * @author Mincong Huang
+ */
+public class ExplicitMappingTest extends ESSingleNodeTestCase {
+
+  @Test
+  public void createIndexWithExplictMapping() {
+    var create =
+        new CreateIndexRequest("my_index")
+            .mapping(
+                "_doc", "{\"properties\":{\"message\":{\"type\":\"text\"}}}", XContentType.JSON);
+    var response = client().admin().indices().create(create).actionGet();
+    Assertions.assertThat(response.isAcknowledged()).isTrue();
+
+    var mappingResponse =
+        client()
+            .admin()
+            .indices()
+            .getFieldMappings(new GetFieldMappingsRequest().indices("my_index").fields("message"))
+            .actionGet();
+    @SuppressWarnings("unchecked")
+    var messageField =
+        (Map<String, Object>)
+            mappingResponse
+                .fieldMappings("my_index", "_doc", "message")
+                .sourceAsMap()
+                .get("message");
+    Assertions.assertThat(messageField).hasSize(1).containsEntry("type", "text");
+  }
+
+  @Test
+  public void addFieldToExistingMapping() {
+    createIndexWithExplictMapping();
+
+    var putMapping =
+        new PutMappingRequest("my_index")
+            .type("_doc")
+            .source("{\"properties\":{\"description\":{\"type\":\"text\"}}}", XContentType.JSON);
+    var response = client().admin().indices().putMapping(putMapping).actionGet();
+    Assertions.assertThat(response.isAcknowledged()).isTrue();
+
+    var mappingResponse =
+        client()
+            .admin()
+            .indices()
+            .getFieldMappings(
+                new GetFieldMappingsRequest().indices("my_index").fields("description"))
+            .actionGet();
+    @SuppressWarnings("unchecked")
+    var descriptionField =
+        (Map<String, Object>)
+            mappingResponse
+                .fieldMappings("my_index", "_doc", "description")
+                .sourceAsMap()
+                .get("description");
+    Assertions.assertThat(descriptionField).hasSize(1).containsEntry("type", "text");
+  }
+}
