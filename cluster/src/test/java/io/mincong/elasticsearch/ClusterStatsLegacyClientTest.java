9b60685f9dfd284b472cfaf6247b034b041cc2f5
Mincong HUANG <mincong.h@gmail.com>
2020-08-01 09:00:05 +0200

    Cluster stats

---
+++ cluster/src/test/java/io/mincong/elasticsearch/ClusterStatsLegacyClientTest.java
@@ -0,0 +1,26 @@
+package io.mincong.elasticsearch;
+
+import org.assertj.core.api.Assertions;
+import org.elasticsearch.test.ESSingleNodeTestCase;
+import org.junit.Test;
+
+public class ClusterStatsLegacyClientTest extends ESSingleNodeTestCase {
+
+  /**
+   * @see <a
+   *     href="https://www.elastic.co/guide/en/elasticsearch/reference/current/cluster-stats.html">Cluster
+   *     stats API</a>
+   */
+  @Test
+  public void getStats() {
+    var client = client();
+
+    // demo:start
+    var clusterStatsResponse = client.admin().cluster().prepareClusterStats().get();
+    // query value of `nodes.fs.available_in_bytes`
+    var availableByteSize = clusterStatsResponse.getNodesStats().getFs().getAvailable();
+    // demo:end
+
+    Assertions.assertThat(availableByteSize.getBytes()).isGreaterThan(0);
+  }
+}
