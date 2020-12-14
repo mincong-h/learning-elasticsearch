83faf4d1e8417c546cba0586182558cbf502ab11
Mincong HUANG <mincong.h@gmail.com>
2020-12-14 15:34:45 +0100

    Fix main

--- demo-dvf/src/main/java/io/mincong/dvf/service/Main.java
+++ demo-dvf/src/main/java/io/mincong/dvf/service/Main.java
@@ -1,23 +1,19 @@
 package io.mincong.dvf.service;
 
 import java.nio.file.Path;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
   public static void main(String[] args) {
     var reader = new TransactionCsvReader();
-    var map = new ConcurrentHashMap<String, String>();
     var count = new AtomicInteger();
     reader
         .readCsv(Path.of("/Users/mincong/github/dvf/downloads/full.2020.csv"))
         .forEach(
             tx -> {
               System.out.println(tx);
-              map.put(tx.id(), tx.id());
               count.incrementAndGet();
             });
     System.out.println("count: " + count);
-    System.out.println("map:   " + map.size());
   }
 }
