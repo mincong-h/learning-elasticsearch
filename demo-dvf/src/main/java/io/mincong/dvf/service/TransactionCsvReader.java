ebd255f0c3be253573badeb2127e162418b7c236
Mincong HUANG <mincong.h@gmail.com>
2020-12-16 09:19:29 +0100

    Implement BulkIterator

--- demo-dvf/src/main/java/io/mincong/dvf/service/TransactionCsvReader.java
+++ demo-dvf/src/main/java/io/mincong/dvf/service/TransactionCsvReader.java
@@ -9,13 +9,14 @@ import io.mincong.dvf.model.ImmutableTransactionRow;
 import io.mincong.dvf.model.TransactionRow;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.List;
-import java.util.Spliterators;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public class TransactionCsvReader {
 
+  private static final int BULK_SIZE = 1_000;
   private final ObjectReader objectReader;
 
   public TransactionCsvReader() {
@@ -27,12 +28,40 @@ public class TransactionCsvReader {
   public Stream<List<ImmutableTransaction>> readCsv(Path path) {
     try {
       MappingIterator<ImmutableTransactionRow> iterator = objectReader.readValues(path.toFile());
-      return StreamSupport.stream(Spliterators.spliteratorUnknownSize(iterator, ORDERED), false)
-          .map(TransactionRow::toTransactionObj)
-          // TODO batching
-          .map(List::of);
+      var bulkIterator = new BulkIterator<>(iterator, BULK_SIZE);
+      return StreamSupport.stream(Spliterators.spliteratorUnknownSize(bulkIterator, ORDERED), false)
+          .map(
+              rows ->
+                  rows.stream().map(TransactionRow::toTransactionObj).collect(Collectors.toList()));
     } catch (IOException e) {
       throw new IllegalStateException("Failed to read file " + path, e);
+    }
+  }
+
+  static class BulkIterator<T> implements Iterator<List<T>> {
+    private final Iterator<T> iterator;
+    private final int bulkSize;
+
+    public BulkIterator(Iterator<T> iterator, int bulkSize) {
+      this.iterator = iterator;
+      this.bulkSize = bulkSize;
+    }
+
+    @Override
+    public boolean hasNext() {
+      return iterator.hasNext();
+    }
+
+    @Override
+    public List<T> next() {
+      var results = new ArrayList<T>(bulkSize);
+      while (hasNext() && results.size() < bulkSize) {
+        results.add(iterator.next());
+      }
+      if (results.isEmpty()) {
+        throw new NoSuchElementException();
+      }
+      return results;
     }
   }
 }
--- demo-dvf/src/test/java/io/mincong/dvf/service/TransactionCsvReaderTest.java
+++ demo-dvf/src/test/java/io/mincong/dvf/service/TransactionCsvReaderTest.java
@@ -3,8 +3,11 @@ package io.mincong.dvf.service;
 import static io.mincong.dvf.model.TestModels.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import io.mincong.dvf.service.TransactionCsvReader.BulkIterator;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.function.Function;
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -22,8 +25,45 @@ public class TransactionCsvReaderTest {
   public void testTransaction() {
     var reader = new TransactionCsvReader();
     assertThat(reader.readCsv(csvPath))
-        .hasSize(9)
+        .hasSize(1)
         .flatExtracting(Function.identity())
         .contains(TRANSACTION_1, TRANSACTION_2, TRANSACTION_3, TRANSACTION_4, TRANSACTION_5);
+  }
+
+  @Test
+  public void testBulkIterator2() {
+    var transactions =
+        List.of(TRANSACTION_1, TRANSACTION_2, TRANSACTION_3, TRANSACTION_4, TRANSACTION_5);
+    var iterator = new BulkIterator<>(transactions.iterator(), 2);
+    Assertions.assertThat(iterator)
+        .toIterable()
+        .containsExactly(
+            List.of(TRANSACTION_1, TRANSACTION_2),
+            List.of(TRANSACTION_3, TRANSACTION_4),
+            List.of(TRANSACTION_5));
+  }
+
+  @Test
+  public void testBulkIterator3() {
+    var transactions =
+        List.of(TRANSACTION_1, TRANSACTION_2, TRANSACTION_3, TRANSACTION_4, TRANSACTION_5);
+    var iterator = new BulkIterator<>(transactions.iterator(), 3);
+    Assertions.assertThat(iterator)
+        .toIterable()
+        .containsExactly(
+            List.of(TRANSACTION_1, TRANSACTION_2, TRANSACTION_3),
+            List.of(TRANSACTION_4, TRANSACTION_5));
+  }
+
+  @Test
+  public void testBulkIterator4() {
+    var transactions =
+        List.of(TRANSACTION_1, TRANSACTION_2, TRANSACTION_3, TRANSACTION_4, TRANSACTION_5);
+    var iterator = new BulkIterator<>(transactions.iterator(), 4);
+    Assertions.assertThat(iterator)
+        .toIterable()
+        .containsExactly(
+            List.of(TRANSACTION_1, TRANSACTION_2, TRANSACTION_3, TRANSACTION_4),
+            List.of(TRANSACTION_5));
   }
 }
