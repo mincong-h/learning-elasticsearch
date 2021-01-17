1117dc053a03c13cfff2e42e8dbf55c80650765b
Mincong Huang <mincong.h@gmail.com>
2021-01-17 17:15:48 +0100

    DVF: Snapshot And Restore (#72)
    * Create snapshot repository and snapshot
    
    * Add restore snapshot request
    
    * Add links

--- demo-dvf/start-docker.sh
+++ demo-dvf/start-docker.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 esdata="${HOME}/dvf-volume-es"
+esbackup="${HOME}/es-backup/demo-dvf/"
 
 if [ ! -d "$esdata" ]
 then
   mkdir -p "$esdata"
+fi
+
+if [ ! -d "$esbackup" ]
+then
+  mkdir -p "$esbackup"
 fi
 
 docker run \
@@ -13,5 +19,7 @@ docker run \
   -p 9300:9300 \
   -e "discovery.type=single-node" \
   -e "cluster.name=es-docker-cluster" \
+  -e "path.repo=/opt/elasticsearch/backup" \
   -v "$esdata":/usr/share/elasticsearch/data \
+  -v "$esbackup":/opt/elasticsearch/backup \
   docker.elastic.co/elasticsearch/elasticsearch:7.10.1
