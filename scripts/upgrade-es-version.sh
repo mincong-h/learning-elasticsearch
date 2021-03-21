081b62cb5730fda35670ddde292b8d763ac6793d
Mincong HUANG <mincong.h@gmail.com>
2021-03-21 19:08:38 +0100

    Find files

--- scripts/upgrade-es-version.sh
+++ scripts/upgrade-es-version.sh
@@ -2,10 +2,19 @@
 #
 # Usage:
 #
-#     scripts/upgrade-es-version.sh <new_version>
+#     scripts/upgrade-es-version.sh <old_version> <new_version>
 #
-# Sample:
+# Sample upgrading from 7.8.0 to 7.10.0:
 #
-#     scripts/upgrade-es-version.sh 7.10.0
+#     scripts/upgrade-es-version.sh 7.8.0 7.10.0
 #
+old_version="$1"
+new_version="$2"
+filepaths=($(rg --files-with-matches --glob "**/*.{xml,yml}" CURRENT_ES_VERSION))
+
+for filepath in "${filepaths[@]}"
+do
+    echo $filepath
+done
+
 echo "Finished."
