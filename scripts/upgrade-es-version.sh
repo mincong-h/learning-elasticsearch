aff569c15b4c613923fd676f6682ee0a8ed00e13
Mincong HUANG <mincong.h@gmail.com>
2021-03-21 21:37:53 +0100

    Change checks

--- scripts/upgrade-es-version.sh
+++ scripts/upgrade-es-version.sh
@@ -10,11 +10,22 @@
 #
 old_version="$1"
 new_version="$2"
+
+if [[ -z $old_version || -z $new_version ]]
+then
+    echo "Missing argument(s). Usage:"
+    echo
+    echo "    upgrade-es-version.sh 7.8.0 7.10.0"
+    echo
+    exit 1
+fi
+
 filepaths=($(rg --files-with-matches --glob "**/*.{xml,yml}" CURRENT_ES_VERSION))
 
 for filepath in "${filepaths[@]}"
 do
-    echo $filepath
+    sed -i '' -e "s/${old_version}/${new_version}/g" $filepath
+    echo "${filepath} done"
 done
 
 echo "Finished."
